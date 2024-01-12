aaf50c3875107333efbb74eba4b7a9b70d8e8084
alexbibich <2marussia2@gmail.com>
2024-01-12 15:41:42 +0300

    Добавлен слайдер

--- block_3/sliders_all.py
+++ block_3/sliders_all.py
@@ -2,18 +2,20 @@ import numpy as np
 import pandas as pd
 from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider
+from matplotlib.widgets import Slider, TextBox
 
 rawData = pd.read_csv('output/profiles.csv', encoding='windows-1251')
 parametersNames = rawData.columns.tolist()[2:]
 [timeLabel, coordLabel] = rawData.columns.tolist()[:2]
 plotsCount = len(parametersNames) 
 
-fig = plt.figure()
+fig = plt.figure(figsize=(8, 5))
+
 axes = [plt.subplot(plotsCount, 1, _ + 1) for _ in range(plotsCount)]
 
 data_skip = len(set(rawData[coordLabel]))
 time_moments = list(set(rawData[timeLabel]))
+time_moments.sort()
 
 def init_func():
     for i in range(len(axes)):
@@ -27,23 +29,34 @@ def init_func():
         yLim = [min(paramData) - 0.1 * (max(paramData) - min(paramData)), max(paramData) + 0.1 * (max(paramData) - min(paramData))]
         axes[i].set_xlim(xLim)
         axes[i].set_ylim(yLim)
-
-def update(step):
-    init_func()
+    ini_draw()
+        
+plots = list()
+def ini_draw(step=0):
     for i in range(len(parametersNames)):
         coordData = rawData[coordLabel]
         paramData = rawData[parametersNames[i]]
-        axes[i].plot(coordData[step * data_skip: (step + 1) * data_skip], paramData[step * data_skip: (step + 1) * data_skip], color='b')
+        plots.append(axes[i].plot(coordData[step * data_skip: (step + 1) * data_skip], paramData[step * data_skip: (step + 1) * data_skip], color='b'))
+
+def change(step):
+    for i in range(len(axes)):
+        paramData = rawData[parametersNames[i]]
+        plots[i][0].set_ydata(paramData[step * data_skip: (step + 1) * data_skip])
+        text_box.set_val(f"{time_moments[step]} c")
 
 init_func()
-update(0)
 
-ax_time = plt.axes([0.15, 0.001, 0.65, 0.03])
-time_slider = Slider(ax_time, 'Steps', 0, len(time_moments), valstep=1)
+ax_time = plt.axes([0.15, 0.0001, 0.5, 0.04])
+time_slider = Slider(ax_time, 'Steps', 0, len(time_moments)-1, valstep=1)
+ax_textbox = fig.add_axes([0.75, 0.001, 0.2, 0.04])
+text_box = TextBox(ax_textbox, "t =", textalignment="center")
+text_box.set_val("0 c")
 
-time_slider.on_changed(update)
+time_slider.on_changed(change)
 
-#plt.subplots_adjust(bottom=0.12)
+plt.subplots_adjust(bottom=0.12)
+#plt.tight_layout()
+
 plt.show()
 
 
